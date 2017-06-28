0edc7e4c7be96e48b41406d5a2169e72005d029d
CalJersey <crippe@gmail.com>
2017-06-28 14:03:13 -0700

    Completed Security Questions

--- starter-code/security_questions.js
+++ starter-code/security_questions.js
@@ -1,1 +1,17 @@
 console.log("security_questions.js loaded");
+
+let securityQuestions = [
+  {question: "What's your name?", expectedAnswer : "Dan"},
+  {question:"How old are you?", expectedAnswer : "Not telling"},
+  {question: "Where do you live?", expectedAnswer : "Oakland"}
+];
+
+for (i=0; i<securityQuestions.length;i++) {
+  promptText = securityQuestions[i].question;
+  let userResponse = console.prompt(promptText);
+  if (userResponse != securityQuestions[i].expectedAnswer) {
+    alert("Sorry wrong answer! Try again");
+    i--;
+  }
+
+}
